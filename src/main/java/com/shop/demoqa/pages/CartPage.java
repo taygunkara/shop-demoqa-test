caedcd0236f1e386b7c84462aaff4331aa482b43
taygunkara <kara.taygun@gmail.com>
2023-12-07 18:39:34 +0300

    Update: CartPage and CartTest

--- src/main/java/com/shop/demoqa/pages/CartPage.java
+++ src/main/java/com/shop/demoqa/pages/CartPage.java
@@ -1,6 +1,7 @@
 package com.shop.demoqa.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage extends BasePage{
@@ -8,25 +9,111 @@ public class CartPage extends BasePage{
         super(driver);
     }
 
-    public static final By cleanCart = By.xpath("a[class=empty-cart]");
     public static final By productName = By.xpath("(//td[@class='product-name'])[1]//a");
-
     public static final By productSize = By.xpath("(//dd[@class='variation-Size'])[1]//p");
     public static final By productCount = By.xpath("(//input[@type='text'])[1]");
+    public static final By pageTitle = By.xpath("//h1[@class='page-title']");
+    public static final By clearShoppingCart = By.xpath("//a[@class='empty-cart']");
+    public static final By emptyCartMessage = By.xpath("//div[@class='cart-empty woocommerce-info']");
+    public static final By updateCartMessage = By.xpath("//div[@class='woocommerce-message']");
+    public static final By productCountIncreaseButton = By.xpath("//button[@class='qty-increase']");
+    public static final By productCountDecreaseButton = By.xpath("//button[@class='qty-decrease']");
+    public static final By updateShoppingCart = By.xpath("//input[@name='update_cart']");
+    public static final By continueShopping = By.xpath("//a[@class='continue']");
+    public static final By couponInput = By.xpath("//input[@id='noo_coupon_code']");
+    public static final By applyCouponButton = By.xpath("//button[@class='noo-apply-coupon']");
+    public static final By couponError = By.xpath("//ul[@class='woocommerce-error']");
+    public static final By proceedCheckout = By.xpath("//a[@class='checkout-button button alt wc-forward']");
+    public static final By removeProduct = By.xpath("//a[@class='icon_close_alt2']");
+    public static final By restoreProduct = By.xpath("//a[@class='restore-item']");
 
     public String getProductCount(){
        return getAttribute(productCount, "value");
     }
-
     public String getProductName(){
         return getText(productName);
     }
-
     public String getProductSize(){
         return getText(productSize);
     }
+    public String getTitle(){
+        return getText(pageTitle);
+    }
 
+    public boolean isOnCartPage(){
+        return getTitle().equals("CART");
+    }
 
+    // CLEAR  ------------------------------------------------
+    public void clearShoppingCart(){
+        clickElement(clearShoppingCart);
+    }
 
+    public String getEmptyMessage(){
+        return getText(emptyCartMessage);
+    }
+    // CLEAR ------------------------------------------------
+
+    // UPDATE ------------------------------------------------
+    public String getUpdateMessage(){
+        return getText(updateCartMessage);
+    }
+
+    public void decreaseProductCount(int decreaseAmount) {
+        for (int i = 0; i < decreaseAmount; i++) {
+            clickElement(productCountDecreaseButton);
+        }
+    }
+
+    public void increaseProductCount(int increaseAmount) {
+        for (int i = 0; i < increaseAmount; i++) {
+            clickElement(productCountIncreaseButton);
+        }
+    }
+
+    public void updateProduct(){
+        increaseProductCount(5);
+        clickElement(updateShoppingCart);
+    }
+    // UPDATE ------------------------------------------------
+
+    // FIXME SAME NAME PROBLEM ?
+    public void continueShopping(){
+        clickElement(continueShopping);
+    }
+
+    // COUPON -------------------------------------------------
+
+    public void applyCoupon(String coupon){
+        type(couponInput, coupon);
+        clickElement(applyCouponButton);
+    }
+
+    public boolean isGetCouponError(){
+        try{
+            return isDisplayed(couponError);
+        } catch (NoSuchElementException e){
+            return false;
+        }
+    }
+
+    // COUPON -------------------------------------------------
+
+    public void goToCheckout(){
+        clickElement(proceedCheckout);
+    }
+
+    // --------------------------------------------------------
+    public void removeProduct() {
+        clickElement(removeProduct);
+    }
+
+    public boolean isProductRemoved(){
+        try{
+            return isDisplayed(restoreProduct);
+        } catch (NoSuchElementException e){
+            return false;
+        }
+    }
 
 }
--- src/test/java/com/shop/demoqa/tests/CartTest.java
+++ src/test/java/com/shop/demoqa/tests/CartTest.java
@@ -2,31 +2,94 @@ package com.shop.demoqa.tests;
 
 import com.shop.demoqa.pages.CartPage;
 import com.shop.demoqa.pages.ProductPage;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CartTest extends BaseTest{
 
-    CartPage cartPage;
-    ProductPage productPage;
+    private CartPage cartPage;
+    private ProductPage productPage;
 
+    // FIXME Cart should not be empty. So in the beginning we need to add product to cart. ?
     @BeforeEach
     public void startTest(){
         setUp();
         navigateToProductPage();
         cartPage = new CartPage(driver);
         productPage = new ProductPage(driver);
-    }
-
-    @Test
-    public void get(){
         productPage.productAddToCart("red", "large", "5");
         productPage.goToCart();
-        System.out.println(cartPage.getProductCount());
-        System.out.println("-------------");
-        System.out.println(cartPage.getProductName());
-        System.out.println("-------------");
-        System.out.println(cartPage.getProductSize());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName("Should Be In Cart Page")
+    @Order(1)
+    public void isOnCartPage(){
+        assertTrue(cartPage.isOnCartPage());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName(" clear cart page")
+    @Order(2)
+    public void isCartPageEmpty(){
+        cartPage.clearShoppingCart();
+        assertEquals("Your cart is currently empty.", cartPage.getEmptyMessage());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName(" adasd")
+    @Order(3)
+    public void CartShouldBeUpdate(){
+        cartPage.updateProduct();
+        assertEquals("Cart updated.", cartPage.getUpdateMessage());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName(" continue ")
+    @Order(4)
+    public void ShouldContinueToShopping(){
+        cartPage.continueShopping();
+        System.out.println(driver.getCurrentUrl());
+        assertEquals("https://shop.demoqa.com/shop/", driver.getCurrentUrl());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName(" apply coupon with error")
+    @Order(5)
+    public void ShouldNotBeDiscountWithInvalidCoupon(){
+        cartPage.applyCoupon("1121232");
+        assertTrue(cartPage.isGetCouponError());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName(" proceed to checkout ")
+    @Order(6)
+    public void ShouldProceedToCheckout(){
+        cartPage.goToCheckout();
+        System.out.println(driver.getCurrentUrl());
+        assertEquals("https://shop.demoqa.com/checkout/", driver.getCurrentUrl());
+    }
+
+    // FIXME RENAME
+    @Test
+    @DisplayName(" remove product ")
+    @Order(7)
+    public void ShouldRemoveProductFromCart(){
+        cartPage.removeProduct();
+        assertTrue(cartPage.isProductRemoved());
+    }
+
+    @AfterEach
+    public void closeTest(){
+        tearDown();
     }
 
 }
