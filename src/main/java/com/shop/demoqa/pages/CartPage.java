d5b0702f89480e1a1236b49c5a262e6ffaf82bb6
taygunkara <kara.taygun@gmail.com>
2023-12-08 17:14:33 +0300

    Update: Removed unused locators and methods

--- src/main/java/com/shop/demoqa/pages/CartPage.java
+++ src/main/java/com/shop/demoqa/pages/CartPage.java
@@ -10,15 +10,12 @@ public class CartPage extends BasePage{
     }
 
     public static final By productName = By.xpath("(//td[@class='product-name'])[1]//a");
-    public static final By productSize = By.xpath("(//dd[@class='variation-Size'])[1]//p");
     public static final By productCount = By.xpath("(//input[@type='text'])[1]");
     public static final By pageTitle = By.xpath("//h1[@class='page-title']");
-    public static final By productCart = By.xpath("//tr[@class='cart_item']");
     public static final By clearShoppingCartButton = By.xpath("//a[@class='empty-cart']");
     public static final By emptyCartMessage = By.xpath("//div[@class='cart-empty woocommerce-info']");
     public static final By updateCartMessage = By.xpath("//div[@class='woocommerce-message']");
     public static final By productCountIncreaseButton = By.xpath("//button[@class='qty-increase']");
-    public static final By productCountDecreaseButton = By.xpath("//button[@class='qty-decrease']");
     public static final By updateShoppingCart = By.xpath("//input[@name='update_cart']");
     public static final By continueShoppingButton = By.xpath("//a[@class='continue']");
     public static final By couponInput = By.xpath("//input[@id='noo_coupon_code']");
